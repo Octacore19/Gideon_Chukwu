42b3abce33d5debc67197260a74c3a080b01fac6
Chukwu Gideon Emeka <chuksgid@gmail.com>
2020-03-04 22:40:13 +0100

    Setup network calls

---
+++ app/src/main/java/com/octacore/gideonchukwu/network/FiltersAPI.kt
@@ -0,0 +1,11 @@
+package com.octacore.gideonchukwu.network
+
+import com.octacore.gideonchukwu.model.RemoteModel.Filters
+import retrofit2.Response
+import kotlinx.coroutines.Deferred
+import retrofit2.http.GET
+
+interface FiltersAPI {
+    @GET("/accounts")
+    fun getFiltersList() : Deferred<Response<Filters>>
+}
---
+++ app/src/main/java/com/octacore/gideonchukwu/network/FiltersService.kt
@@ -0,0 +1,30 @@
+package com.octacore.gideonchukwu.network
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object FiltersService {
+    private const val BASE_URL = "https://android-json-test-api.herokuapp.com/"
+
+    fun createService(): FiltersAPI = getRetrofitService().create(FiltersAPI::class.java)
+
+    fun getRetrofitService() : Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .client(getClient())
+        .build()
+
+    private fun getClient() : OkHttpClient{
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+}
